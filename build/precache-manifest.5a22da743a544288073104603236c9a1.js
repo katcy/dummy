28f28e0b251dd10021140ac4a48d5f80d47f508d
karthik chinnasamy <kchinnasamy@wealthengine.com>
2020-07-14 13:45:00 +0530

    initial commit

---
+++ build/precache-manifest.5a22da743a544288073104603236c9a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d3a9daebfb51b2165e66e38a9b8bc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4ddc5ecb4a4a876bb90",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a088942867e18fbec46",
+    "url": "/static/js/2.05a0b5ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05a0b5ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ddc5ecb4a4a876bb90",
+    "url": "/static/js/main.07457c5d.chunk.js"
+  },
+  {
+    "revision": "8ad9931a40bb71d696eb",
+    "url": "/static/js/runtime-main.a47f312a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
